c71c511c577fe14fa78330e718bb12042b32bb00
spirillen <44526987+spirillen@users.noreply.github.com>
2020-02-04 02:55:21 +0100

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -35,23 +35,10 @@ printf "\nMySql Import DONE\n"
 # ***************
 # Import via AXFR
 # ***************
-printf "\nImporting AXFR\n"
-
-#AXFRImport () {
-	#truncate -s 0 "${testfile}"
-	
-    	#dig axfr rpz.mypdns.cloud @axfr.ipv4.mypdns.cloud -p 5353 +noidnin +noidnout \
-		#| grep -F "CNAME" | grep -vE "(^(\*\.|$))" \
-		#| sed 's/\.rpz\.mypdns\.cloud.*$//;s/^\s*\(.*[^ \t]\)\(\s\+\)*$/\1/' \
-		#> "${testfile}"
-
-	#printf "\nImporting AXFR... DONE!\n"
-	##exit ${?}
-#}
-#AXFRImport
+printf "\nImporting RPZ database for adult.mypdns.cloud\n"
 
 ImportRPZ () {
-	mysql -u"${imp_user}" -p"${imp_pw}" -h"${imp_host}" -B -N -e "SELECT TRIM(TRAILING '.rpz.mypdns.cloud' FROM name) AS name FROM pdns.records WHERE \`type\` = 'CNAME' AND domain_id = '36' AND content = '.' LIMIT 10000000;" > "${testfile}"
+	mysql -u"${imp_user}" -p"${imp_pw}" -h"${imp_host}" -B -N -e "SELECT TRIM(TRAILING '.rpz.mypdns.cloud' FROM name) AS name FROM pdns.records WHERE \`type\` = 'CNAME' AND domain_id = '61' AND content = '.' LIMIT 10000000;" > "${testfile}"
 }
 ImportRPZ
 
